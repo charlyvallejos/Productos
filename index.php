fd4c13263d5559919b73f332b1d74923f96b7520
matilda <matilda@charly>
2020-06-23 22:20:28 -0300

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -10,14 +10,16 @@
     </head>
     <body ng-controller="productosController">        
         <h1>Bienvenido mother fucker</h1>
-        <button ng-click="logout()">Cerrar Sesion</button>
-        
+        <div ng-controller="loginController">
+            <button ng-click="logout()">Cerrar Sesion</button>
+        </div>
     
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script> 
     <script src="js/angular.min.js"></script>
     <script src="js/dirPagination.js"></script>
-    <script src="js/app.js"></script>
     <script src="js/login.js"></script>
+    <script src="js/app.js"></script>
+    
     <script src="https://code.angularjs.org/1.3.0-rc.2/angular-messages.js"></script>    
     </body>
 </html>
--- js/app.js
+++ js/app.js
@@ -1,21 +1,6 @@
 var app = angular.module('appProductos',['angularUtils.directives.dirPagination','ngMessages','appLogin'])
         .controller('productosController',function($scope,$http,$window){
-            $scope.logout = function(){
-                var link = "";
-                link = apiURL + '?logout';
-                
-                $http({
-                   method:'GET',
-                   url: link
-                })
-                .then(function(resp){
-                    $window.location.reload();
-                })
-                .catch(function(resp){
-                    console.log(resp);
-                });
-                        
-            };
+//            
         });
 
 
